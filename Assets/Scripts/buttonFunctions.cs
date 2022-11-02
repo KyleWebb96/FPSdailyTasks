416e7a7afc86051e3ec57d652ffabd62bd60f54a
KyleWebb96 <k.webb_bis@yahoo.com>
2022-11-01 18:45:09 -0700

    fixing git

---
+++ Assets/Scripts/buttonFunctions.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonFunctions : MonoBehaviour
+{
+    public void resume()
+    {
+        gameManager.instance.unPauseGame();
+        gameManager.instance.pauseMenu.SetActive(false);
+        gameManager.instance.isPaused = false;
+    }
+
+    public void restart()
+    {
+        gameManager.instance.unPauseGame();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void respawn()
+    {
+        gameManager.instance.unPauseGame();
+        gameManager.instance.playerScript.respawn();
+    }
+}
